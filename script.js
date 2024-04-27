3cb6a16bf48eacdf9bed8718ab4b041025aa21b5
Arunkumar032024 <arunkumar032024@gmail.com>
2024-04-27 17:16:27 +0530

    Add content

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 "use strict";
-b = 10;
-console.log(b);
+let inputValue = document.querySelector(".inputdiv input");
+const addBtn = document.querySelector(".inputdiv button"),
+nightBtn = document.querySelector('.inputdiv .fa-moon'),
+dayBtn = document.querySelector('.inputdiv .fa-sun');
+
+// addBtn.addEventListener('click', () => {
+    
+// })
+nightBtn.addEventListener('click', () => {
+    document.querySelector('.container').style.background = "#000";
+    document.querySelector('.container').style.color = "#fff";
+    nightBtn.classList.remove('active');
+    dayBtn.classList.add('active');
+})
+dayBtn.addEventListener('click', () => {
+    document.querySelector('.container').style.background = "#fff";
+    document.querySelector('.container').style.color = "#000";
+    dayBtn.classList.remove('active');
+    nightBtn.classList.add('active');
+})
+
